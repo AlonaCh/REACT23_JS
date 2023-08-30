bb7b88adb9bfbab3c2ce241cdc8e7e8067cd5a6e
Chubenko Alona <s2301193@BC-C02J64ATQ6LRs-MacBook-Air.local>
2023-08-30 09:45:18 +0300

    finished JS in the gasoline calculator

--- task4/main.js
+++ task4/main.js
@@ -1,4 +1,24 @@
-const price = document.querySelector('#price').value
+const calcGasoline = () => {
+    const price = document.querySelector('#price').value
 const money = document.querySelector('#money').value
+const answer = document.querySelector('#answer')
 
-console.log(price);
+const amount = (money / price).toFixed(3);
+
+
+
+amount >= 10 ? text = `You can get ${amount} liters, good, you can escape now` : text = `You can get ${amount} liters, Ups, you have to stay here`
+
+answer.textContent = text; 
+
+/* if (amount >= 10){
+    text = `You can get ${amount} liters, good, you can escape now`;
+} else {
+    text = `You can get ${amount} liters, Ups, you have to stay here`;
+} */
+
+// answer.textContent = text;
+}
+
+
+
