174bdca333ea100c2d304a5e71016ba303fedfcb
Alona Chubenko <aliona.chubenko@gmail.com>
2023-09-11 17:04:04 +0300

    Finished JS using eventListener, querySelector, forEach Loop

--- PizzaOrderApp/main.js
+++ PizzaOrderApp/main.js
@@ -1,37 +1,82 @@
-// Welcome part
+/* Welcome part
+function name {
+    return name.value
+} */
 
+//Price depending on size
 
+let pizzaPrice = 0;
+let pizzaToppingValue = 0;
+let pizzaDeliveryValue = 0;
 
-// Used .eventListener method and .querySelector to define a price depending on the pizza size
-let priceTwo = 7;
-let priceFour = 10;
-let priceSix = 12;
-let priceEight = 14;
+const pizzaSizePrices = document.querySelectorAll("input[type=radio ]");
+console.log(pizzaSizePrices);
 
-function priceDefinedTwo() {
-const showPrice = document.querySelector("#showPrice");
-showPrice.textContent = priceTwo;
+pizzaSizePrices.forEach((pizzaSize) => {
+  pizzaSize.addEventListener("click", () => {
+    if (pizzaSize.value === "twoServings") {
+        pizzaSizeValue = 7;
+      pizzaPrice =
+      Number(pizzaSizeValue) +
+      Number(pizzaToppingValue) +
+      Number(pizzaDeliveryValue);
+    } else if (pizzaSize.value === "fourServings") {
+        pizzaSizeValue = 10;
+      pizzaPrice =
+      Number(pizzaSizeValue) +
+      Number(pizzaToppingValue) +
+      Number(pizzaDeliveryValue);
+    } else if (pizzaSize.value === "sixServings") {
+        pizzaSizeValue = 12;
+      pizzaPrice =
+      Number(pizzaSizeValue) +
+      Number(pizzaToppingValue) +
+      Number(pizzaDeliveryValue);
+    } else if (pizzaSize.value === "eightServings") {
+        pizzaSizeValue = 16;
+      pizzaPrice =
+        Number(pizzaSizeValue) +
+        Number(pizzaToppingValue) +
+        Number(pizzaDeliveryValue);
+    }
+    document.querySelector("#showPrice").textContent = pizzaPrice;
+  });
+});
+// Price depending on the topping
+
+    const toppingsOption = document.querySelectorAll("input[name='addTop']");
+  
+    toppingsOption.forEach((topping) => {
+        
+        topping.addEventListener('click', () => {
+            let quantityTopping = 0;
+            for (let i = 0; i < toppingsOption.length; i++) {
+            if (toppingsOption[i].checked) {
+                quantityTopping++;
+              } }
+              if (quantityTopping >= 4) {
+                pizzaToppingValue = (quantityTopping - 4) * 0.5;
+    
+        document.querySelector("#showPrice").textContent =
+        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
+        } else {
+            document.querySelector("#showPrice").textContent =
+            pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
+        }
+    })
+});
+        // Delivery price
+        const deliveryPrice = document.querySelector('#delivery');
+function newPizzaPrice(){
+    if (deliveryPrice.value === "deliveryHome") {
+        pizzaDeliveryValue = 5
+        document.querySelector("#showPrice").textContent = 
+        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
+    } else {
+        pizzaDeliveryValue = 0
+        document.querySelector("#showPrice").textContent = 
+        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
+    }
 }
-function priceDefinedFour() {
-    const showPrice = document.querySelector("#showPrice");
-    showPrice.textContent = priceFour;  
-}
-function priceDefinedSix() {
-    const showPrice = document.querySelector("#showPrice");
-    showPrice.textContent = priceSix;  
-}
-function priceDefinedEight() {
-    const showPrice = document.querySelector("#showPrice");
-    showPrice.textContent = priceEight;  
-}
+        deliveryPrice.addEventListener('click', newPizzaPrice);
 
-let priceForTwo = document.querySelector("#two").addEventListener("click", priceDefinedTwo);
-
-let priceForFour = document.querySelector("#four");
-priceForFour.addEventListener("click", priceDefinedFour);
-
-let priceForSix = document.querySelector("#six");
-priceForSix.addEventListener("click", priceDefinedSix);
-
-let priceForEight = document.querySelector("#eight");
-priceForEight.addEventListener("click", priceDefinedEight);
