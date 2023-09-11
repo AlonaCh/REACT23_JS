ccec8445eb2250dd487363b1c522fc25596b49be
Alona Chubenko <aliona.chubenko@gmail.com>
2023-09-11 22:08:50 +0300

    Modified CSS and HTML in PizzaOrderApp

--- PizzaOrderApp/main.js
+++ PizzaOrderApp/main.js
@@ -1,8 +1,3 @@
-/* Welcome part
-function name {
-    return name.value
-} */
-
 //Price depending on size
 
 let pizzaPrice = 0;
@@ -15,68 +10,67 @@ console.log(pizzaSizePrices);
 pizzaSizePrices.forEach((pizzaSize) => {
   pizzaSize.addEventListener("click", () => {
     if (pizzaSize.value === "twoServings") {
-        pizzaSizeValue = 7;
-      pizzaPrice =
-      Number(pizzaSizeValue) +
-      Number(pizzaToppingValue) +
-      Number(pizzaDeliveryValue);
-    } else if (pizzaSize.value === "fourServings") {
-        pizzaSizeValue = 10;
-      pizzaPrice =
-      Number(pizzaSizeValue) +
-      Number(pizzaToppingValue) +
-      Number(pizzaDeliveryValue);
-    } else if (pizzaSize.value === "sixServings") {
-        pizzaSizeValue = 12;
-      pizzaPrice =
-      Number(pizzaSizeValue) +
-      Number(pizzaToppingValue) +
-      Number(pizzaDeliveryValue);
-    } else if (pizzaSize.value === "eightServings") {
-        pizzaSizeValue = 16;
+      pizzaSizeValue = 7;
       pizzaPrice =
         Number(pizzaSizeValue) +
         Number(pizzaToppingValue) +
-        Number(pizzaDeliveryValue);
+        Number(pizzaDeliveryValue) + ' € ';
+    } else if (pizzaSize.value === "fourServings") {
+      pizzaSizeValue = 10;
+      pizzaPrice =
+        Number(pizzaSizeValue) +
+        Number(pizzaToppingValue) +
+        Number(pizzaDeliveryValue) + ' € ';
+    } else if (pizzaSize.value === "sixServings") {
+      pizzaSizeValue = 12;
+      pizzaPrice =
+        Number(pizzaSizeValue) +
+        Number(pizzaToppingValue) +
+        Number(pizzaDeliveryValue) + ' € ';
+    } else if (pizzaSize.value === "eightServings") {
+      pizzaSizeValue = 16;
+      pizzaPrice =
+        Number(pizzaSizeValue) +
+        Number(pizzaToppingValue) +
+        Number(pizzaDeliveryValue) + ' € ';
     }
     document.querySelector("#showPrice").textContent = pizzaPrice;
   });
 });
 // Price depending on the topping
 
-    const toppingsOption = document.querySelectorAll("input[name='addTop']");
-  
-    toppingsOption.forEach((topping) => {
-        
-        topping.addEventListener('click', () => {
-            let quantityTopping = 0;
-            for (let i = 0; i < toppingsOption.length; i++) {
-            if (toppingsOption[i].checked) {
-                quantityTopping++;
-              } }
-              if (quantityTopping >= 4) {
-                pizzaToppingValue = (quantityTopping - 4) * 0.5;
-    
-        document.querySelector("#showPrice").textContent =
-        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
-        } else {
-            document.querySelector("#showPrice").textContent =
-            pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
-        }
-    })
-});
-        // Delivery price
-        const deliveryPrice = document.querySelector('#delivery');
-function newPizzaPrice(){
-    if (deliveryPrice.value === "deliveryHome") {
-        pizzaDeliveryValue = 5
-        document.querySelector("#showPrice").textContent = 
-        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
-    } else {
-        pizzaDeliveryValue = 0
-        document.querySelector("#showPrice").textContent = 
-        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue;
-    }
-}
-        deliveryPrice.addEventListener('click', newPizzaPrice);
+const toppingsOption = document.querySelectorAll("input[name='addTop']");
 
+toppingsOption.forEach((topping) => {
+  topping.addEventListener("click", () => {
+    let quantityTopping = 0;
+    for (let i = 0; i < toppingsOption.length; i++) {
+      if (toppingsOption[i].checked) {
+        quantityTopping++;
+      }
+    }
+    if (quantityTopping >= 4) {
+      pizzaToppingValue = (quantityTopping - 4) * 0.5;
+
+      document.querySelector("#showPrice").textContent =
+        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue  + ' € ';
+    } else {
+      document.querySelector("#showPrice").textContent =
+        pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue + ' € ';
+    }
+  });
+});
+// Delivery price
+const deliveryPrice = document.querySelector("#delivery");
+function newPizzaPrice() {
+  if (deliveryPrice.value === "deliveryHome") {
+    pizzaDeliveryValue = 5;
+    document.querySelector("#showPrice").textContent =
+      pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue + ' € ';
+  } else {
+    pizzaDeliveryValue = 0;
+    document.querySelector("#showPrice").textContent =
+      pizzaSizeValue + pizzaToppingValue + pizzaDeliveryValue + ' € ';
+  }
+}
+deliveryPrice.addEventListener("click", newPizzaPrice);
