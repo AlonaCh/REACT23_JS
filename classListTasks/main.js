8d37ea796d37b093d30fb8dc67f915e73567a056
Alona Chubenko <aliona.chubenko@gmail.com>
2023-09-19 12:56:46 +0300

    add responsive  mobile menu

--- classListTasks/main.js
+++ classListTasks/main.js
@@ -1,10 +1,30 @@
 const backButton = document.querySelector('#backToTop');
+const mobButton = document.querySelector('.mobile');
+const nav = document.querySelector('nav ul')
+
+
+// browsers onscroll event triger
+window.onscroll = function() {scrollFunction()};
+// code form W3S about scrolling, for two browsers
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backButton.style.display = "block";
+  } else {
+    backButton.style.display = "none";
+  }
+}
 
 const getToTop = () => {
 document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
-
+}
+const mobMenu = () => {
+if (nav.classList.contains('responsive')) {
+    nav.classList.remove('responsive') 
+    } else {
+        nav.classList.add('responsive')
+    }
 }
 
-
 backButton.addEventListener('click', getToTop);
+mobButton.addEventListener('click', mobMenu);
